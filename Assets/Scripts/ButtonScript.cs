9472a36bd493ee243ba4bcfde046cc747a229ea5
LAPTOP-HKLP8R3J\kota2 <kota.2199.yuuki@gmail.com>
2018-04-18 18:16:25 +0900

    retry

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+
+    [SerializeField] PushedButtonScript gimmick;
+
+    void Start()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            gimmick.OnButtonPushed();
+        }
+    }
+}
